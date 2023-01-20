7d4de4bbb86e3141ede73d5a39c8766069a91d5d
Denishilo <denisanishenko@gmail.com>
2023-01-20 22:22:23 +0300

    refactoring, fix css

--- src/components/searchField/searchField.tsx
+++ src/components/searchField/searchField.tsx
@@ -1,51 +1,15 @@
 import SearchIcon from "@mui/icons-material/Search";
 import {styled} from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
-import s from './Input.module.css'
-
-// const Search = styled('div')((theme) => ({
-//     position: 'relative',
-//     borderRadius: '5px',
-//     border: 'solid 1px #DEDBDC',
-//     width: '672px',
-//     height:'40px',
-// }));
-
-// const SearchIconWrapper = styled('div')(({theme}) => ({
-//     paddingLeft: '16px',
-//     height: '100%',
-//     position: 'absolute',
-//     pointerEvents: 'none',
-//     display: 'flex',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     color: '#ADABAC',
-// }));
-
-// export const SearchField = () => {
-//     return (
-//         <Search>
-//             <SearchIconWrapper>
-//                 <SearchIcon/>
-//             </SearchIconWrapper>
-//             <InputBase
-//                 className={s.input}
-//                 placeholder="Search…"
-//                 inputProps={{'aria-label': 'search'}}/>
-//         </Search>
-//     );
-// }
 
 const Search = styled('div')(({theme}) => ({
     border: 'solid 1px #DEDBDC',
     position: 'relative',
     borderRadius: '5px',
-    // width: '665px',
     [theme.breakpoints.up('sm')]: {
         marginLeft: theme.spacing(1),
         width: '665px',
-        height: '36px',
-        margin:'0px'
+        margin:'0px',
     },
 }));
 
@@ -63,7 +27,7 @@ const SearchIconWrapper = styled('div')(({theme}) => ({
 const StyledInputBase = styled(InputBase)(({theme}) => ({
     color: 'inherit',
     '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
+        padding: theme.spacing("12px", 1, 1, 0),
         paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     },
     width: '100%',
--- src/components/selectSort/selectSort.tsx
+++ src/components/selectSort/selectSort.tsx
@@ -1,7 +1,9 @@
+import React from "react";
 import MenuItem from '@mui/material/MenuItem';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import s from './SelectSort.module.css'
+import FormControl from "@mui/material/FormControl";
+import {Select} from "@mui/material";
+import {styled} from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
 
 const values = [
     {
@@ -19,24 +21,53 @@ const values = [
 
 ];
 
-export const SelectSort = () => {
+const BootstrapInput = styled(InputBase)(({ theme }) => ({
+    'label + &': {
+    },
+    '& .MuiInputBase-input': {
+        width:'256px',
+        borderRadius: 4,
+        position: 'relative',
+        backgroundColor: "#FAF7F8",
+        border: '1px solid #ced4da',
+        fontSize: 16,
+        padding: '10px 26px 10px 12px',
+        transition: theme.transitions.create(['border-color', 'box-shadow']),
+        // Use the system font instead of the default Roboto font.
+        fontFamily: [
+            "Inter, sans-serif"
+        ].join(','),
+        '&:focus': {
+            borderRadius: 4,
+            backgroundColor: "#FAF7F8",
+        },
+    },
+}));
+
+export const SelectSort =()=> {
+    const [value, setAge] = React.useState('');
+    const handleChange = (event: { target: { value: string } }) => {
+        setAge(event.target.value);
+    };
     return (
-            <Box width='256px'  component="form">
-                <div>
-                    <TextField className={s.text}
-                               id="outlined-select-currency"
-                               select
-                               defaultValue="1"
-                               fullWidth
-                               sx = {{width:'256px', height:'36px'}}
-                    >
-                        {values.map((option) => (
+        <div>
+            <FormControl sx={{ m: 1, marginRight:'0px'}} variant="standard">
+                <Select
+
+                    labelId="demo-customized-select-label"
+                    id="demo-customized-select"
+                    value={value}
+                    onChange={handleChange}
+                    inputProps={{MenuProps: {disableScrollLock: true}}}
+                    input={<BootstrapInput />}
+                >
+                    {values.map((option) => (
                             <MenuItem key={option.value} value={option.value}>
                                 {option.label}
                             </MenuItem>
-                        ))}
-                    </TextField>
-                </div>
-            </Box>
-    )
+                         ))}
+                </Select>
+            </FormControl>
+        </div>
+    );
 }
